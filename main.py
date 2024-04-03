df635f62fc6bfea117a9c83d89200f2cd5db0ab9
Piyush Arora <arora.8@iitj.ac.in>
2024-04-03 16:06:28 +0530

    punjabi added

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ warnings.filterwarnings("ignore")
 
 from module.models import modelDic
 from module.utils import verification
-from utils import load_checkpoint, EER_data
+from utils import load_checkpoint, EER_data, vox, punjabi
 
 parser = argparse.ArgumentParser()
 
@@ -16,7 +16,7 @@ parser.add_argument('--model', type=str, default='wavlm_base_plus')
 parser.add_argument('--checkpoint', type=str, default='checkpoints/wavlm_base_plus_nofinetune.pth')
 parser.add_argument('--audio1', type=str, default='data/1.m4a')
 parser.add_argument('--audio2', type=str, default='data/2.m4a')
-parser.add_argument('--que', type=str, default="1.c")
+parser.add_argument('--que', type=str, default="1.d")
 
 args = parser.parse_args()
 
@@ -33,5 +33,15 @@ if args.que == "1.b":
 elif args.que == "1.c":
     model = modelDic[args.model]
     model = load_checkpoint(model, args.checkpoint)
-    eer = EER_data(model)
+    data = vox(16000, pathfile='data/veri_test.txt')
+    print("Size of the dataset: {}".format(len(data)))
+    eer = EER_data(model, data)
+    print("EER: {:.4f}".format(eer))
+
+elif args.que == "1.d":
+    model = modelDic[args.model]
+    model = load_checkpoint(model, args.checkpoint)
+    data = punjabi(4000, pathfile='data/kathbath/punjabi_meta/test_known_data.txt')
+    print("Size of the dataset: {}".format(len(data)))
+    eer = EER_data(model, data)
     print("EER: {:.4f}".format(eer))
--- utils.py
+++ utils.py
@@ -1,13 +1,12 @@
 import torch
 import torchaudio
-import numpy as np
-import torchaudio.transforms as T
 from module.models import ECAPA_TDNN_SMALL
 from module.utils import verification
 from sklearn import metrics
 from scipy.optimize import brentq
 from scipy.interpolate import interp1d
 from tqdm import tqdm
+import librosa
 
 def load_checkpoint(model, checkpoint):
     model = ECAPA_TDNN_SMALL(**model)
@@ -54,9 +53,58 @@ class vox(torch.utils.data.Dataset):
         
         return audio1[0], audio2[0], self.labels[idx]
 
-def EER_data(model):
+class punjabi(torch.utils.data.Dataset):
+    def __init__(self, cropLength, pathfile, partition='test_known'):
+        self.cropLength = cropLength
+        self.pathfile = pathfile
+        self.partition = partition
+        self.DATA = 'data/kathbath/kb_data_clean_m4a/punjabi'
+        self.audio1_list = []
+        self.audio2_list = []
+        self.labels = []
 
-    data = vox(16000, pathfile='data/veri_test.txt')
+        file = open(pathfile, 'r')
+        for line in file:
+            line = line.strip().split()
+            self.labels.append(int(line[0]))
+            self.audio1_list.append(line[1])
+            self.audio2_list.append(line[2])
+        
+        for i in range(len(self.audio1_list)):
+            self.audio1_list[i] = self.audio1_list[i].split('punjabi')[1]
+            self.audio1_list[i] = self.audio1_list[i].split('/')
+            self.audio1_list[i] = self.partition + '/' + self.audio1_list[i][2] + '/' + self.audio1_list[i][4]
+            self.audio1_list[i] = self.DATA + '/' + self.audio1_list[i]
+            self.audio1_list[i] = self.audio1_list[i].replace('.wav', '.m4a')
+        
+        for i in range(len(self.audio2_list)):
+            self.audio2_list[i] = self.audio2_list[i].split('punjabi')[1]
+            self.audio2_list[i] = self.audio2_list[i].split('/')
+            self.audio2_list[i] = self.partition + '/' + self.audio2_list[i][2] + '/' + self.audio2_list[i][4]
+            self.audio2_list[i] = self.DATA + '/' + self.audio2_list[i]
+            self.audio2_list[i] = self.audio2_list[i].replace('.wav', '.m4a')
+
+    def __len__(self):
+        return len(self.audio1_list)
+    
+    def __getitem__(self, idx):
+        audio1, sr1 = librosa.load(self.audio1_list[idx])
+        audio2, sr2 = librosa.load(self.audio2_list[idx])
+
+        audio1 = torch.tensor(audio1, dtype=torch.float32).unsqueeze(0)
+        audio2 = torch.tensor(audio2, dtype=torch.float32).unsqueeze(0)
+        
+        if audio1.size(1) < self.cropLength:
+            audio1 = torch.nn.functional.pad(audio1, (0, self.cropLength - audio1.size(1)))
+        if audio2.size(1) < self.cropLength:
+            audio2 = torch.nn.functional.pad(audio2, (0, self.cropLength - audio2.size(1)))
+        
+        audio1 = audio1[:,:self.cropLength]
+        audio2 = audio2[:,:self.cropLength]
+        
+        return audio1[0], audio2[0], self.labels[idx]
+        
+def EER_data(model, data):
 
     model = model.eval()
     dataloader = torch.utils.data.DataLoader(data, batch_size=125, shuffle=False)
